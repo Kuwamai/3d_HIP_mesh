0fbc1c0ff0c7f5a3374e4698377417b3caed67d6
Kuwamai <mitsumex333@gmail.com>
2018-09-10 18:55:29 +0900

    add new files

---
+++ create_galaxy.py
@@ -0,0 +1,19 @@
+import numpy as np
+import os
+import bpy
+
+filepath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../hippa_pos.txt'))
+pos = np.loadtxt(filepath)
+
+mesh = bpy.data.meshes.new('Galaxy_mesh')
+obj = bpy.data.objects.new('Galaxy', mesh)
+bpy.context.scene.objects.link(obj)
+
+faces = []
+for i in range(len(pos)-2):
+    faces.append([i, i+1, i+2])
+faces.append([len(pos)-2, len(pos)-1, 0])
+faces.append([len(pos)-1, 0, 1])
+
+mesh.from_pydata(pos, [], faces)
+mesh.update()
